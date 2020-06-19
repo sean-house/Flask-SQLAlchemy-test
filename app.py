a6c21b61dd9a45411b929a366cf70c953a872535
Sean House <33880135+sean-house@users.noreply.github.com>
2020-06-19 13:55:05 +0100

    Added user activation

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_restful import Api
 from flask_jwt_extended import JWTManager
 from marshmallow import ValidationError
 import os
+import sys
 
 from resources.user import UserRegister, UserList, UserLogin, TokenRefresh, UserConfirm
 from resources.measurement import Measurement, MeasurementList
@@ -16,8 +17,9 @@ app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
     "FLASK_DB_URL", "sqlite:///dbase.db"
 )
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["JWT_SECRET_KEY"] = "qwertyuiop"
-app.secret_key = "qwertyuiop"
+secret_key = os.environ.get('FLASK_SECRET', "qwertyuiop")
+app.config["JWT_SECRET_KEY"] = secret_key
+app.secret_key = secret_key
 api = Api(app)
 
 jwt = JWTManager(app)
@@ -45,4 +47,14 @@ api.add_resource(UserConfirm, "/confirm/<int:user_id>")
 if __name__ == "__main__":
     db.init_app(app)
     ma.init_app(app)
-    app.run(port=5000, debug=True)  # important to mention debug=True
+    intent = os.environ.get('FLASK_INTENT', None)
+    if intent == 'dev':
+        print('Running with "dev" environment')
+        app.run(port=5000, debug=True, use_reloader=False)  # important to mention debug=True
+    elif intent == 'prod':
+        print('Running with "prod" environment')
+        app.run(host='0.0.0.0', port=5000, debug=False)
+    else:
+        print('ERROR:  No FLASK_INTENT environment variable')
+        sys.exit(-1)
+
